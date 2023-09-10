fca6490e597ab421b4985e0fbea255b5171545bd
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-09-10 12:55:01 +0300

    Optimize CSS Animations for Smoother User Interactions

---
+++ NewDesign/DEVELOPER/AngularJS_version/public/build/components/language/languageSelector.js
@@ -0,0 +1,1 @@
+define(["app"],function(a){"use strict";a.registerDirective("languageSelector",["Language",function(){return{restrict:"EA",replace:!0,templateUrl:"build/components/language/language-selector.tpl.html",scope:!0}}])});
