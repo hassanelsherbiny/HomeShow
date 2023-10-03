132a00226e2a9dfecaa35132590f12a4c2ce2422
Hasan Elsherbiny <HassanElsherbiny55@Gmail.com>
2023-10-03 18:41:02 +0300

    Update Third-Party Plugins for Improved Efficiency

---
+++ NewDesign/DEVELOPER/AngularJS_version/public/app/modules/forms/directives/form-layouts/smartRegistrationForm.js
@@ -0,0 +1,84 @@
+define(['modules/forms/module', 'modules/forms/common', 'jquery-maskedinput', 'jquery-validation'], function (module, formsCommon) {
+
+    'use strict';
+
+    return module.registerDirective('smartRegistrationForm', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'app/modules/forms/directives/form-layouts/smart-registration-form.tpl.html',
+            scope: true,
+            link: function (scope, form) {
+
+                form.validate(angular.extend({
+
+                    // Rules for form validation
+                    rules: {
+                        username: {
+                            required: true
+                        },
+                        email: {
+                            required: true,
+                            email: true
+                        },
+                        password: {
+                            required: true,
+                            minlength: 3,
+                            maxlength: 20
+                        },
+                        passwordConfirm: {
+                            required: true,
+                            minlength: 3,
+                            maxlength: 20,
+                            equalTo: '#password'
+                        },
+                        firstname: {
+                            required: true
+                        },
+                        lastname: {
+                            required: true
+                        },
+                        gender: {
+                            required: true
+                        },
+                        terms: {
+                            required: true
+                        }
+                    },
+
+                    // Messages for form validation
+                    messages: {
+                        login: {
+                            required: 'Please enter your login'
+                        },
+                        email: {
+                            required: 'Please enter your email address',
+                            email: 'Please enter a VALID email address'
+                        },
+                        password: {
+                            required: 'Please enter your password'
+                        },
+                        passwordConfirm: {
+                            required: 'Please enter your password one more time',
+                            equalTo: 'Please enter the same password as above'
+                        },
+                        firstname: {
+                            required: 'Please select your first name'
+                        },
+                        lastname: {
+                            required: 'Please select your last name'
+                        },
+                        gender: {
+                            required: 'Please select your gender'
+                        },
+                        terms: {
+                            required: 'You must agree with Terms and Conditions'
+                        }
+                    }
+
+                }, formsCommon.validateOptions));
+
+            }
+        }
+    });
+});
